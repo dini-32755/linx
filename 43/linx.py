a6f84129e782dafcffd27f7138f2955492720257
dini-32755 <118438047+dini-32755@users.noreply.github.com>
2023-02-23 09:06:02 +0000

    Update linx.py

--- 43/linx.py
+++ 43/linx.py
@@ -1,5 +1,5 @@
 import time, sys, os                                                                                                                                                                                                                                                                                          
-#print('ola')
+#print('ola'
 
 print('\033[32mrun-user1╰(*°▽°*)╯\033[m')
 
